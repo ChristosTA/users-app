c09c34003e8cbc6a3809205489f14f5ec251fe47
Christos Tartaris <xristosbnd@gmail.com>
2025-04-19 11:04:12 +0300

    added google auth login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,6 +4,16 @@ const router = express.Router();
 const authController = require('../controllers/auth.controller')
 
 router.post('/login', authController.login)
+router.get('/google/callback', authController.googleLogin);
 
 
 module.exports = router
+
+
+
+
+//https://accounts.google.com/o/oauth2/auth?client_id=923012922602-ue4br55qo3dhihvspq72lo9i7ofiehdt.apps.googleusercontent.com&redirect_uri=http://localhost:3000/api/auth/google/callback&response_type=code&scope=email%20profile&access_type=offline
+
+
+
+//https://accounts.google.com/o/oauth2/auth?client_id=923012922602-ue4br55qo3dhihvspq72lo9i7ofiehdt.apps.googleusercontent.com&redirect_uri=http://localhost:3000/api/auth/google/callback&response_type=code&scope=email%20profile&access_type=offline
